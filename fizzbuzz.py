53accf037d8575436ff2b02c7517d70a5e4efc8f
sunguangning <sunguangning@uninopay.com>
2020-01-15 15:29:37 +0800

    fizzbuzz first commit

---
+++ fizzbuzz.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+import sys
+import getopt
+
+def main():
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "h", ["help"])
+        print_fizzbuzz(int(args[0]))
+    except:
+        sys.exit(0)
+
+
+def print_fizzbuzz(input_num):
+    for i in range(1, input_num+1):
+        if fizzbuzz(i) != "pass":
+            print("%s %s" % (i, fizzbuzz(i)))
+
+
+def fizzbuzz(input_num):
+    if input_num % 3 == 0 and input_num % 5 != 0:
+        return ("fizz")
+    elif input_num % 3 != 0 and input_num % 5 == 0:
+        return ("buzz")
+    elif input_num % 3 == 0 and input_num % 5 == 0:
+        return ("fizz buzz")
+    else:
+        return "pass"
+    
+
+
+if __name__ == "__main__":
+    main()
+
---
+++ fizzbuzz_test.py
@@ -0,0 +1,5 @@
+from fizzbuzz import fizzbuzz
+
+
+def test_fizzbuzz():
+    assert fizzbuzz(3) == 'fizz'
